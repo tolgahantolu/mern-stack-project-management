0d56e07fa759f211d425058fdc28f5c670a0db5c
Tolgahan Tolu <tolgahanntolu@gmail.com>
2022-11-05 10:52:24 +0300

    mongoDB connection

--- server/index.js
+++ server/index.js
@@ -2,9 +2,13 @@ const express = require("express");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+// Connect to database
+connectDB();
 
 app.use(
   "/graphql",
