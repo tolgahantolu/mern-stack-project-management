fe2ef9590a3dfc8c1bead1d63dc1f12617d0ada7
Tolgahan Tolu <tolgahanntolu@gmail.com>
2022-11-07 21:44:15 +0300

    setting apollo & fetching clients

---
+++ client/src/components/Clients.jsx
@@ -0,0 +1,35 @@
+import { useQuery } from "@apollo/client";
+import { GET_CLIENTS } from "../queries/clientsQueries";
+import ClientRow from "./ClientRow";
+import Spinner from "./Spinner";
+
+const Clients = () => {
+  const { loading, error, data } = useQuery(GET_CLIENTS);
+
+  if (loading) return <Spinner />;
+  if (error) return <p>Someting Went Wrong!</p>;
+
+  return (
+    <>
+      {!loading && !error && (
+        <table className="table table-hover mt-3">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.clients.map((client) => {
+              return <ClientRow key={client.id} client={client} />;
+            })}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
+};
+
+export default Clients;
