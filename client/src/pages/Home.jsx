31c5ac6baac0fd32466d813eb901f96439abc148
Tolgahan Tolu <tolgahanntolu@gmail.com>
2022-11-09 20:20:14 +0300

    add project mutation and delete project

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import AddClientModal from "../components/AddClientModal";
+import AddProjectModal from "../components/AddProjectModal";
 import Clients from "../components/Clients";
 import Projects from "../components/Projects";
 
 const Home = () => {
   return (
     <>
-      <AddClientModal />
+      <div className="d-flex gap-4">
+        <AddClientModal />
+        <AddProjectModal />
+      </div>
       <Projects />
       <hr />
       <Clients />
--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,5 +1,7 @@
 import { useQuery } from "@apollo/client";
 import { Link, useParams } from "react-router-dom";
+import ClientInfo from "../components/ClientInfo";
+import DeleteProjectButton from "../components/DeleteProjectButton";
 import Spinner from "../components/Spinner";
 import { GET_PROJECT } from "../queries/projectQueries";
 
@@ -28,6 +30,10 @@ const Project = () => {
 
           <h5 className="mt-3">Project Status</h5>
           <p className="lead">{data.project.status}</p>
+
+          <ClientInfo client={data.project.client} />
+
+          <DeleteProjectButton projectId={data.project.id} />
         </div>
       )}
     </>
